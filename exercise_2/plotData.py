f7cf2c297d5839e7cb9d1f2704cc58fbb5e9425b
zachduey <zachduey@gmail.com>
2015-05-25 08:57:05 -0500

    Updated ex2.py

---
+++ exercise_2/plotData.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+def plotData(X, y)
+
+    """
+    Plots the data points X and y into a new figure with + for the positive
+    examples and 0 for the negative examples. X is assumed to be an Mx2 array.
+
+    """
+
+
+
