1f7be6b3a470ab2896c3458d56a029e00cecf849
zachduey <zachduey@gmail.com>
2015-05-28 09:18:46 -0500

    Updated plotting and predict functions

--- exercise_2/ex2_utils.py
+++ exercise_2/ex2_utils.py
@@ -26,7 +26,7 @@ def plotData(X, y):
     # Specified in plot order
     ax.legend(['Admitted', 'Not admitted'])
 
-    return fig
+    return (fig, ax)
 
 def costFunction(theta,X,y):
     """
@@ -74,37 +74,40 @@ def plotDecisionBoundary(theta, X, y):
        intercept.
     2) MxN, N>3 matrix, where the first column is all-ones
     """
-
-plotData(X(:,2:3), y)
-
-if size(X, 2) <= 3
-    # Only need 2 points to define a line, so choose two endpoints
-    plot_x = [min(X(:,2))-2,  max(X(:,2))+2]
-
-    # Calculate the decision boundary line
-    plot_y = (-1./theta(3)).*(theta(2).*plot_x + theta(1))
-
-    # Plot, and adjust axes for better viewing
-    plot(plot_x, plot_y)
     
-    # Legend, specific for the exercise
-    legend('Admitted', 'Not admitted', 'Decision Boundary')
-    axis([30, 100, 30, 100])
-else
-    # Here is the grid range
-    u = linspace(-1, 1.5, 50)
-    v = linspace(-1, 1.5, 50)
+    # Start with same plot as before
+    fig, ax = plotData(X[:,1:], y)
 
-    z = zeros(length(u), length(v))
-    # Evaluate z = theta*x over the grid
-    for i = 1:length(u)
-        for j = 1:length(v)
-            z(i,j) = mapFeature(u(i), v(j))*theta
-        end
-    end
-    z = z' # important to transpose z before calling contour
+    """
+    if size(X, 2) <= 3
+	# Only need 2 points to define a line, so choose two endpoints
+	plot_x = [min(X(:,2))-2,  max(X(:,2))+2]
 
-    # Plot z = 0
-    # Notice you need to specify the range [0, 0]
-    contour(u, v, z, [0, 0], 'LineWidth', 2)
+	# Calculate the decision boundary line
+	plot_y = (-1./theta(3)).*(theta(2).*plot_x + theta(1))
 
+	# Plot, and adjust axes for better viewing
+	plot(plot_x, plot_y)
+	
+	# Legend, specific for the exercise
+	legend('Admitted', 'Not admitted', 'Decision Boundary')
+	axis([30, 100, 30, 100])
+    else
+	# Here is the grid range
+	u = linspace(-1, 1.5, 50)
+	v = linspace(-1, 1.5, 50)
+
+	z = zeros(length(u), length(v))
+	# Evaluate z = theta*x over the grid
+	for i = 1:length(u)
+	    for j = 1:length(v)
+		z(i,j) = mapFeature(u(i), v(j))*theta
+	    end
+	end
+	z = z' # important to transpose z before calling contour
+
+	# Plot z = 0
+	# Notice you need to specify the range [0, 0]
+	contour(u, v, z, [0, 0], 'LineWidth', 2)
+    """
+    return fig, ax
