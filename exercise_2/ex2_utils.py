429f48ddf35290eccb83ab13dac193312b919ab4
zachduey <zachduey@gmail.com>
2015-05-28 08:44:00 -0500

    Added prediction function

--- exercise_2/ex2_utils.py
+++ exercise_2/ex2_utils.py
@@ -17,7 +17,7 @@ def plotData(X, y):
     neg = X[np.where(y==0)]
 
     fig, ax = plt.subplots()
-    ax.plot(pos[:,0],pos[:,1],"y+",neg[:,0],neg[:,1],"bo")
+    ax.plot(pos[:,0],pos[:,1],"k+",neg[:,0],neg[:,1],"yo")
 
     # Labels and Legend
     ax.set_xlabel('Exam 1 score')
@@ -28,29 +28,83 @@ def plotData(X, y):
 
     return fig
 
-"""
 def costFunction(theta,X,y):
-    
-    Computs  using sum of square errors for linear 
-    regression using theta as the parameter vector for 
-    linear regression to fit the data points in X and y.
+    """
+    Computes loss using sum of square errors for logistic regression
+    using theta as the parameter vector for linear regression to fit 
+    the data points in X and y.
 
-    Note: Requires numpy in order to run, but is not imported as part of this
-	  script since it is imported in ex1.py and therefore numpy is part of
-	  the namespace when the function is actually run.
+    """
+
     # Initialize some useful values
     m = len(y) # number of training examples
     
     # Cost function J(theta)
-    J = (np.sum((np.dot(X,theta) - y)**2))/(2*m)
+    J =(np.sum(-y*np.log(sigmoid(np.dot(X,theta)))-
+       (1-y)*(np.log(1-sigmoid(np.dot(X,theta))))))/m
 
-
-    return
-"""
+    # Gradient
+    grad = (np.sum((sigmoid(np.dot(X,theta))-y)[:,None]*X,axis=0))/m
+  
+    return (J, grad)
 
 def sigmoid(z):
     """ Returns the value from using x in the sigmoid function """
     
     return 1.0/(1 +  np.e**(-z))
 
+
+def predict(theta,X):
+    """
+    Given a vector of parameter results and training set X,
+    returns the model prediction for admission. If predicted
+    probability of admission is greater than .5, predict will
+    return a value of 1.
+    """
+    return np.where(np.dot(X,theta) > 5.,1,0)
+
+
+
+def plotDecisionBoundary(theta, X, y):
+    """	
+    Plots the data points X and y into a new figure with the decision boundary
+    defined by theta with + for the positive examples and o for the negative 
+    examples. X is assumed to be a either 
+    1) Mx3 matrix, where the first column is an all-ones column for the 
+       intercept.
+    2) MxN, N>3 matrix, where the first column is all-ones
+    """
+
+plotData(X(:,2:3), y)
+
+if size(X, 2) <= 3
+    # Only need 2 points to define a line, so choose two endpoints
+    plot_x = [min(X(:,2))-2,  max(X(:,2))+2]
+
+    # Calculate the decision boundary line
+    plot_y = (-1./theta(3)).*(theta(2).*plot_x + theta(1))
+
+    # Plot, and adjust axes for better viewing
+    plot(plot_x, plot_y)
+    
+    # Legend, specific for the exercise
+    legend('Admitted', 'Not admitted', 'Decision Boundary')
+    axis([30, 100, 30, 100])
+else
+    # Here is the grid range
+    u = linspace(-1, 1.5, 50)
+    v = linspace(-1, 1.5, 50)
+
+    z = zeros(length(u), length(v))
+    # Evaluate z = theta*x over the grid
+    for i = 1:length(u)
+        for j = 1:length(v)
+            z(i,j) = mapFeature(u(i), v(j))*theta
+        end
+    end
+    z = z' # important to transpose z before calling contour
+
+    # Plot z = 0
+    # Notice you need to specify the range [0, 0]
+    contour(u, v, z, [0, 0], 'LineWidth', 2)
 
